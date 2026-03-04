fbb94ebe21e872c95407c9dd5e0099093c583f89
kingnurbek8-hue <kingnurbek8@gmail.com>
2026-03-04 16:08:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import telebot
 from telebot import types
 
+# O'z tokeningizni ehtiyotkorlik bilan qo'ying
 TOKEN = '8743861596:AAFN5YiHbRscPHxTwd_c2r0C6CzyM1gwzCw'
 bot = telebot.TeleBot(TOKEN)
 
-
 @bot.message_handler(commands=['start'])
 def start(message):
+    # Diqqat! Bu yerga o'zingizning Render saytingiz manzilini yozing
+    # Masalan: https://nft-gift-bot.onrender.com
+    web_app_url = "https://SAYTINGIZ_MANZILI.onrender.com"
     
-    web_app_url = "https://https://t.me/OnyxCratebot"
-    
-
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("🚀 START GAME", web_app=types.WebAppInfo(web_app_url)))
+    # Chat ichidagi asosiy tugma
+    markup.add(types.InlineKeyboardButton("🚀 ENTER GAME", web_app=types.WebAppInfo(web_app_url)))
     
-    
-    bot.set_chat_menu_button(message.chat.id, types.MenuButtonWebApp("Play", types.WebAppInfo(web_app_url)))
-    
-    bot.send_message(message.chat.id, "<b>Welcome!</b>\nPress the button below to start.", parse_mode="HTML", reply_markup=markup)
+    # Pastdagi menyu tugmasini "Play" ga aylantirish
+    try:
+        bot.set_chat_menu_button(message.chat.id, types.MenuButtonWebApp("Play", types.WebAppInfo(web_app_url)))
+    except:
+        pass
+
+    bot.send_message(message.chat.id, 
+        "<b>Welcome to Premium Pepe Case!</b>\n\nClick the button below to start the game.", 
+        parse_mode="HTML", reply_markup=markup)
 
 bot.polling(none_stop=True)
