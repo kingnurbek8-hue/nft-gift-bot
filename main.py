1a045c05ae9b8d1d57e183df3e53d3ad40735b99
kingnurbek8-hue <kingnurbek8@gmail.com>
2026-03-04 16:24:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 import telebot
 from telebot import types
 
-# O'z tokeningizni ehtiyotkorlik bilan qo'ying
 TOKEN = '8743861596:AAFN5YiHbRscPHxTwd_c2r0C6CzyM1gwzCw'
 bot = telebot.TeleBot(TOKEN)
 
+# DIQQAT: Linkni oxiridagi / bilan birga yozing
+WEB_URL = "https://kingnurbek8-hue.github.io/nft-gift-bot/"
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    # Diqqat! Bu yerga o'zingizning Render saytingiz manzilini yozing
-    # Masalan: https://nft-gift-bot.onrender.com
-    web_app_url = "https://kingnurbek8-hue.github.io/nft-gift-bot/"
-
     markup = types.InlineKeyboardMarkup()
-    # Chat ichidagi asosiy tugma
-    markup.add(types.InlineKeyboardButton("🚀 ENTER GAME", web_app=types.WebAppInfo(web_app_url)))
+    # WebApp tugmasi
+    markup.add(types.InlineKeyboardButton("🚀 Play Epic Gift", web_app=types.WebAppInfo(WEB_URL)))
     
-    # Pastdagi menyu tugmasini "Play" ga aylantirish
-    try:
-        bot.set_chat_menu_button(message.chat.id, types.MenuButtonWebApp("Play", types.WebAppInfo(web_app_url)))
-    except:
-        pass
-
+    # Pastdagi Menu tugmasini ham Play qilib qo'yamiz
+    bot.set_chat_menu_button(message.chat.id, types.MenuButtonWebApp("Play", types.WebAppInfo(WEB_URL)))
+    
     bot.send_message(message.chat.id, 
-        "<b>Welcome to Premium Pepe Case!</b>\n\nClick the button below to start the game.", 
+        "<b>Welcome to Epic Gift Clone!</b>\n\nClick the button below to start.", 
         parse_mode="HTML", reply_markup=markup)
 
 bot.polling(none_stop=True)
